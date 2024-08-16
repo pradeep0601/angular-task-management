3e576fa6324d56c328a09fb500eaee69f18572c6
Pradeep Kumar <pradeepk.pdp@gmail.com>
2024-08-15 19:04:57 -0500

    feat(component) header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'task-management' title`, () => {
+  it(`should have the 'angular-task-management' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-management');
+    expect(app.title).toEqual('angular-task-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task-management');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-task-management');
   });
 });
